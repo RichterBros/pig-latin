5088fee0bdf73c00e13aa01702dadd465eb624b1
Erich Richter and Jieun Kang <jieun@>
2020-01-23 15:47:51 -0800

    add additional js

--- js/scripts.js
+++ js/scripts.js
@@ -9,27 +9,78 @@
 
 //user interface logic
 $(document).ready(function() {
-    $("#formOne").submit(function(event) {
-      event.preventDefault();
-      var input = $("input#input").val();
-      var wordArr = input.split(' ');
-
-      console.log(wordArr);
-
-     var newArr =[]
-      newArr = wordArr.map(function(word) {
-          return word.charAt(0)
-        
-        
-          
-          
-    });      
-    console.log(newArr);
-}); 
-
-    //   var result = 
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    // var wordArr =[];
+      
+    var input = $("input#input").val();
+      // var wordArr = input.split(' ');
      
-    //   $("#result").text(result);
-      //console.log(wordArr);
-});
-  
+    var vowels = ["a", "e", "i", "o", "u" ];
+      vowels.forEach(function(vowel){
+        if (input.charAt(0) === vowel) {
+          $("#result").text(input + "way")
+        }
+      });
+    var num = 2
+    
+    //console.log(slipByChar);
+    var vowels = ["a", "e", "i", "o", "u" ];
+    
+    var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+    var splitByChar = input//.split("") 
+    var sliced =[];
+    consonants.forEach(function(consonant) {
+      
+      //sliced = splitByChar.slice(0, 2);
+      
+      if (input.charAt(0) === consonant){
+
+       sliced = splitByChar.slice(0, 3);
+      }
+      
+      
+      // if (input.charAt(0) === consonant && input.charAt(1) === consonant && input.charAt(2) === consonant) {
+      //   sliced = splitByChar.slice(0, 2);
+      
+      // } else if (input.charAt(0) === consonant && input.charAt(1) === consonant) {
+      //   sliced =  splitByChar.slice(0, 1);
+
+      // } else if (input.charAt(0) === consonant) {
+      //   sliced =  splitByChar.slice(0);
+      // }
+
+        
+        
+        
+      //}
+      
+      
+      
+     });
+     
+     console.log(sliced);
+    });
+         
+        //   var sliceCon = input.slice(num);
+        //   $("#result").text(sliceCon + "ay")
+        // }
+          //slicedArr = input.slice(0,2);
+       
+       
+        
+          
+        
+        //$("#result").text(input + "ay")
+      });
+      //console.log(resultCon) 
+    
+
+            
+       
+      
+        
+        
+     
+        
+
